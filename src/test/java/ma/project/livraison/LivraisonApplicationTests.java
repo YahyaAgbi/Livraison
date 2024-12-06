c93dd583a583c44ea67ed930bb3a8c6c8e936507
yahyaagbi <yahyaagbi12@gmail.com>
2024-12-06 10:47:42 +0100

    V1 Livraison

---
+++ src/test/java/ma/project/livraison/LivraisonApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.project.livraison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivraisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
